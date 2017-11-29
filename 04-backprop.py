1579786259603a8553da9171fe16027639ce5e6a
Izzet Mustafaiev <izzet@mustafaiev.com>
2017-11-29 18:32:59 +0200

    Code for backpropogation

---
+++ 04-backprop.py
@@ -0,0 +1,47 @@
+#%%
+
+import torch
+from torch.autograd import Variable
+
+#%%
+
+
+x_data = [1.0, 2.0, 3.0]
+y_data = [2.0, 4.0, 6.0]
+
+#%%
+
+# any random value
+w = Variable(torch.Tensor([1.0]), requires_grad=True)
+
+
+def forward(x):
+    return x * w
+
+
+def loss(x, y):
+    y_pred = forward(x)
+    return (y_pred - y) * (y_pred - y)
+
+
+print("predict (before training)", 4, forward(4).data[0])
+
+
+#%%
+
+for epoch in range(1000):
+    for x_val, y_val in zip(x_data, y_data):
+        l = loss(x_val, y_val)
+        l.backward()
+        print("\tgrad:", x_val, y_val, w.grad.data[0])
+        w.data = w.data - 0.01 * w.grad.data
+
+        # manually zero gradients after updating weights
+        w.grad.data.zero_()
+    print("progress:", epoch, l.data[0])
+
+# after training
+print("predict (after training)", 4, forward(4).data[0])
+
+#%%
+print("predict (after training)", 5, forward(5).data[0])
